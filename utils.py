962a660433a4b6ccdd0eaf6632e625dbd4878563
OriJor <70166337+OriJor@users.noreply.github.com>
2023-06-05 20:52:05 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,5 @@
 import torch
 
 
-def accuracy(outputs,labels):
-    preds = outputs.argmax(-1)
-    acc = (preds == labels.view_as(preds)).float().detach().numpy().mean()
-    return acc
-    
-
 def save_model(model, path):
     torch.save(model.state_dict(), path)
